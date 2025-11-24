1ec0f65a1228222942bc6d66350866179511983d
esraaSoffar77 <esss.edu@gmail.com>
2025-11-24 10:32:30 +0200

    Added Invoice Line

---
+++ invoice-line.py
@@ -0,0 +1,13 @@
+from base import BaseModel
+
+class InvoiceLine(BaseModel):
+    def _init_(self, name, product, quantity, unit_price, invoice):
+        super()._init_(name)
+        self.product = product
+        self.quantity = quantity
+        self.unit_price = unit_price
+        self.invoice = invoice
+
+    @property
+    def subtotal(self):
+        return self.quantity * self.unit_price
